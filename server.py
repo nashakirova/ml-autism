fe1a04ecd7ac59a85a338f1eab29591a0b06f4ca
Nailya Shakirova <nailya.shakirova@foam.agency>
2024-08-01 20:38:29 +0200

    Fix the testing script

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ class SaveModelStrategy(fl.server.strategy.FedAvg):
 strategy = SaveModelStrategy(
    fraction_fit=1.0,  
    fraction_evaluate=1.0, 
-   min_fit_clients=2,  
+   min_fit_clients=1,  
    min_evaluate_clients=1, 
    min_available_clients=1, 
    evaluate_metrics_aggregation_fn=weighted_average
--- test_of_sequential_stuff.py
+++ test_of_sequential_stuff.py
@@ -1,6 +1,9 @@
 import subprocess
 for i in range(5,10):
     print(f"iteration {i}")
-    subprocess.Popen('python server.py', shell=True)
-    subprocess.Popen(f"python client.py {i}", shell=True)
-    exit(0)
+    server = subprocess.Popen('python server.py', shell=True)
+    client = subprocess.Popen(f"python client.py {i}", shell=True)
+    server.wait()
+    client.wait()
+    print('HERE')
+    #exit(0)
